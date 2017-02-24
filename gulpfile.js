90bc69e5c69c4154e73352082ae6d329f3de5736
Jindřich Kuba <jindrich.kuba@indigo.cz>
2017-02-24 14:38:24 +0100

    first commit
    Signed-off-by: Jindřich Kuba <jindrich.kuba@indigo.cz>

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp'),
+    replacebatch = require('gulp-batch-replace'),
+    replace = require('gulp-replace'),
+    htmlmin = require('gulp-htmlmin');
+    csvParser = require('gulp-csv-to-json-array'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('csv', function(cb) {
+  gulp.src(['*.csv'])
+        .pipe(csvParser({
+            dynamicTyping: true,
+            skipEmptyLines: true
+        }))
+        .pipe(gulp.dest("./"));
+        setTimeout(function () {
+     console.log('dokonceno')
+     cb()
+   }, 100)
+});
+
+gulp.task('json', ['csv'], function() {
+  return gulp.src('preklad.json', { base: './' })
+     .pipe(htmlmin({collapseWhitespace: true}))
+     .pipe(replace('["','[">'))
+     .pipe(replace('","','<",">'))
+     .pipe(replace('"]','<"]'))
+     .pipe(gulp.dest('./'));
+});
+
+var replaceThis = require('./preklad.json');
+
+gulp.task('html', ['json'], function(){
+  return gulp.src('*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(replacebatch(replaceThis))
+    .pipe(gulp.dest('build'))
+});
+
+gulp.task('browser-sync', ['html'], function() {
+     browserSync.init({
+         server: {
+             baseDir: "./build"
+         }
+     });
+});
+
+gulp.task('default', ['csv', 'json', 'html', 'browser-sync']);
